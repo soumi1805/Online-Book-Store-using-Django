8e2042d5be208ce520038539a1b4092dbb902370
Soumi Sen <38695267+soumi1805@users.noreply.github.com>
2019-11-21 22:11:47 +0530

    Add files via upload

---
+++ orders/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    price=item['price'],
+                    quantity=item['quantity']
+                )
+            cart.clear()
+        return render(request, 'created.html', {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'create.html', {'form': form})
---
+++ shop/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock', 'available', 'created_at', 'updated_at']
+    list_filter = ['available', 'created_at', 'updated_at']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Product, ProductAdmin)
